dfb25897224af0ab7856f83bef336cd7baed5fea
Arthur <crfernandesarthur@gmail.com>
2024-10-19 13:38:15 -0300

    rota create_user

--- app.py
+++ app.py
@@ -39,5 +39,20 @@ def logout():
   
   return jsonify({"message": "Logout realizado  com sucesso"})
 
+
+@app.route('/user', methods=['POST'])
+def create_user():
+  data = request.json
+  username = data.get('username')
+  password = data.get('password')
+  
+  if username and password:
+    user = User(username= username, password= password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'Usuario cadastrado com sucesso.'})
+  
+  return jsonify({'message': 'Dados invalidos.'}), 400
+
 if __name__ == '__main__':
   app.run(debug= True)
